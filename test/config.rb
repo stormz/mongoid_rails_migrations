84cfb337ab68f7862f43386aa96b0efd5f03864c
guille-moe <coder@guille.moe>
2017-05-19 10:47:07 +0200

    Edit config for testing mongoid with specific hosts and db.

--- test/config.rb
+++ test/config.rb
@@ -1,7 +1,15 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'mongoid_rails_migrations')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'rails', 'generators', 'mongoid', 'mongoid_generator')
 
-Mongoid.configure.connect_to('mongoid_test')
+# Mongoid.configure.connect_to('mongoid_test')
+Mongoid.configure.load_configuration({
+  clients: {
+    default: {
+      database: ENV['MONGOID_DB'] || "mongoid_test",
+      hosts: [ ENV['MONGOID_HOST'] || "localhost:27017" ],
+    }
+  }
+})
 
 # require all models
 Dir[File.join(File.dirname(__FILE__), 'models', '*.rb')].each { |file| require file }
